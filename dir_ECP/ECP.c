247211e1884d416e3386ee667fe831074c1dc808
lidiamcfreitas <lidiamcfreitas@gmail.com>
2015-09-18 14:51:03 +0100

    organized The Stuff

---
+++ dir_ECP/ECP.c
@@ -0,0 +1,20 @@
+//
+//  ECP.c
+//  
+//
+//  Created by Lídia Maria Carvalho de Freitas on 17/09/15.
+//
+//
+
+#include "User.h"
+
+int main(int argc, char *argv[]){
+    
+    int sock_fd;
+    struct sockaddr_in servAddr, clntAddr;
+    unsigned int clntAddrLen;
+
+
+    
+
+}
---
+++ dir_ECP/ECP.h
@@ -0,0 +1,14 @@
+//
+//  ECP.h
+//  
+//
+//  Created by Lídia Maria Carvalho de Freitas on 17/09/15.
+//
+//
+
+#ifndef ____ECP__
+#define ____ECP__
+
+#include <stdio.h>
+
+#endif /* defined(____ECP__) */
