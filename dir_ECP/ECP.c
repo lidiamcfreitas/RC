b1418808fceb542f75a1591f4ffa8552c1811548
lidiamcfreitas <lidiamcfreitas@gmail.com>
2015-09-28 02:22:37 +0100

    ECP: iqr and awi working

--- dir_ECP/ECP.c
+++ dir_ECP/ECP.c
@@ -2,7 +2,6 @@
 //  ECP.c
 //  
 //
-//  Created by Lídia Maria Carvalho de Freitas on 17/09/15.
 //
 //
 
@@ -21,16 +20,15 @@ int main(int argc, char *argv[]){
     char topic_ip[16];
     unsigned short topic_port;
     char to_send[2600];
+    int i;
     
     struct TES{
         char QName[255];
         char TESIp[16];
         unsigned short TESPort;  
-    };
+    }TES_servers[99];
+    int num_TES = 0;
 
-    struct TES TES_servers[99];
-    
-    
     if(argc==1){
         servPort = DEFAULT_PORT;
     } else if(argc == 3 && (strcmp(argv[1], "-p") == 0)){
@@ -54,15 +52,15 @@ int main(int argc, char *argv[]){
     
     if((file_ptr = fopen("dir_ECP/topics.txt", "r"))==NULL)
         DieWithError("fopen() failed");
-    
-    int i;
+
     i = 0;
-    while(fscanf(file_ptr, "%s %s:%hu", topic_name, topic_ip, &topic_port)!=EOF){
+    while(fscanf(file_ptr, "%s %[^:]:%hu", topic_name, topic_ip, &topic_port)==3){
         strcpy(TES_servers[i].QName, topic_name);
         strcpy(TES_servers[i].TESIp,  topic_ip);
         TES_servers[i].TESPort = topic_port;
         i++;
-        printf("%s %s:%d", topic_name, topic_ip, topic_port);
+        num_TES++;
+        printf("read: %s %s %hu \n", topic_name, topic_ip, topic_port); 
     }
     fclose(file_ptr);    
 
@@ -71,7 +69,84 @@ int main(int argc, char *argv[]){
     for(;;){ 
         if((recvStringLen = recvfrom(sock_fd, buffer, sizeof(buffer), 0, (struct sockaddr*) &clntAddr, &clntAddrLen))<0)
             DieWithError("recvfrom() failed");
-        if (recvStringLen == strlen("TQR\n")){ /*when an TQR is received */
+        buffer[recvStringLen] = '\0';
+
+        /* TQR */
+        if (strcmp("TQR\n",buffer)==0 && (recvStringLen==strlen("TQR\n")) /*because of possible double \0*/)
+        {
+            strcpy(to_send, "AWT T");
+            sprintf(topic_name, "%d ", num_TES); /* using topic_name just to reuse variable */
+            strcat(to_send, topic_name);
+            if (num_TES==0)
+            { /* No topics available */
+                if (sendto(sock_fd, "EOF\n", strlen("EOF\n"), 0, (struct sockaddr*) &clntAddr, sizeof(clntAddr))<0)
+                    DieWithError("sendto() failed");
+            } else {
+                for(i=0; i<num_TES; i++)
+                {
+                    strcat(to_send, TES_servers[i].QName);
+                    strcat(to_send, " ");
+                    printf("%s\n", to_send);
+                }
+                to_send[strlen(to_send)-1] = '\n'; /* substitute ' ' for '\n'" */
+                if (sendto(sock_fd, to_send, strlen(to_send), 0, (struct sockaddr*) &clntAddr, sizeof(clntAddr))<0)
+                    DieWithError("sendto() failed");
+            }
+        }
+        /* TER */
+        else if(!strncmp(buffer, "TER T", 5) && (recvStringLen==strlen("TER T1\n") || recvStringLen==strlen("TER T99\n"))){
+            sscanf(buffer, "TER T%d", &i);
+            i--; /* 1...99 to 0..98 */
+            if(i<0 || i>99){
+                if (sendto(sock_fd, "EOF\n", strlen("EOF\n"), 0, (struct sockaddr*) &clntAddr, sizeof(clntAddr))<0)
+                    DieWithError("sendto() failed");
+            } else {
+                strcpy(to_send, "AWTES ");
+                strcat(to_send, TES_servers[i].TESIp);
+                strcat(to_send, " ");
+                sprintf(topic_name, "%d", TES_servers[i].TESPort); /* using topic_name just to reuse variable */
+                strcat(to_send, topic_name);
+                strcat(to_send, "\n");
+
+                if (sendto(sock_fd, to_send, strlen(to_send), 0, (struct sockaddr*) &clntAddr, sizeof(clntAddr))<0)
+                    DieWithError("sendto() failed");
+            }
+        }
+        /* IQR */
+        else if(!strncmp(buffer, "IQR ", 4)){
+            
+            char iqr[4];
+            char qid[255];
+            int sid;
+            char topname[26];
+            int score;
+
+            FILE *f = fopen("dir_ECP/stats.txt", "a");
+            if (f == NULL)
+                DieWithError("Error opening file: stats.txt");
+            
+             //memmove(buffer, buffer+4, strlen(buffer) - 4 + 1);
+
+            if(sscanf(buffer,"%s %d %s %s %d",iqr, &sid, qid, topname, &score)!=5)
+            {
+                if (sendto(sock_fd, "ERR\n", strlen("ERR\n"), 0, (struct sockaddr*) &clntAddr, sizeof(clntAddr))<0)
+                    DieWithError("sendto() failed");
+            }
+            fprintf(f, "%d %s %s %d", sid, qid, topname, score);
+
+            fclose(f);
+
+            strcpy(to_send, "AWI ");
+            strcat(to_send, qid);
+
+            if (sendto(sock_fd, to_send, strlen(to_send), 0, (struct sockaddr*) &clntAddr, sizeof(clntAddr))<0)
+                DieWithError("sendto() failed");
+        }
+        /* else -> ERR*/
+        else
+        {
+            if (sendto(sock_fd, "ERR\n", strlen("ERR\n"), 0, (struct sockaddr*) &clntAddr, sizeof(clntAddr))<0)
+                DieWithError("sendto() failed");
         }
         printf("Received %sFrom %s:%d\n", buffer, inet_ntoa(clntAddr.sin_addr),ntohs(clntAddr.sin_port));
 
