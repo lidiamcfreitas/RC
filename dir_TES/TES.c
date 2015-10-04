c5e5e0ac2edabbffe069f554bc9b29a5cb73e0b3
Miguel Vera <Miguel.coimbra.vera@gmail.com>
2015-10-04 22:43:26 +0100

    TES update

--- dir_TES/TES.c
+++ dir_TES/TES.c
@@ -26,6 +26,8 @@ int main(int argc, char *argv[]){
     unsigned short ecp_port;
     int recv_string_len;
     FILE *file_ptr;
+    FILE *answers_ptr;
+    FILE *user_info_ptr;
     unsigned short topic_port;
     char* ecp_name;
     char* read_buffer;
@@ -33,6 +35,13 @@ int main(int argc, char *argv[]){
     char buffer[32];
     char* command;
 
+    typedef struct user_info{
+        long QID = 0;
+        int SID  0;
+        char time_limit[19];
+    } user_info;
+
+    struct usr_info user_array[99];
     if(argc==5){
         server_port = atoi(argv[1]);
         ecp_name = argv[2];
@@ -57,7 +66,11 @@ int main(int argc, char *argv[]){
     	DieWithError("bind() failed");
 
     if((file_ptr = fopen("dir_ECP/topics.pdf", "r"))==NULL)
-        DieWithError("fopen() failed");
+        DieWithError("topics fopen() failed");
+    if((answers_ptr = fopen("dir_TES/answers.txt", "r"))==NULL)
+        DieWithError("answers fopen() failed");
+    if((user_info_ptr = fopen("dir_TES/user_info.txt", "a"))==NULL)
+        DieWithError("user_info fopen() failed");
 
     long file_size, block_size, bytes_sent, bytes_left;
     char* ptr;
@@ -67,26 +80,65 @@ int main(int argc, char *argv[]){
         client_addr_len = sizeof(client_addr);
         new_fd = accept(sock_fd, (struct sockaddr*)&client_addr, &client_addr_len);
 
-        read_buffer = tcpread_nbytes(new_fd, 10);
+        read_buffer = tcpread_nbytes(new_fd, 4);
         printf("Received %sFrom %s:%d\n", read_buffer, inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
         command = strtok(read_buffer, " ");
         printf("Command: %s\n", command);
-        if (strcmp(command, "RQS")==0){
-            printf("I LOVE COCAINE\n", command);
 
+        /*RQS*/
+        if (strcmp(command, "RQS") == 0){
+            char* user_answer;
+            char answers[4];
+            int i = 0;
+            long QID, SID;
+
+            while(fscanf(user_info_ptr, "%ld %d %s", QID, SID, read_buffer)==3){
+                strcpy(user_array[i].time_limit, read_buffer);
+                i++;
+                printf("read: %ld %d %s \n", QID, SID, read_buffer);
+            }
+
+            fscanf(answers_ptr, "%s %s %s %s %s", answers[0],answers[1],answers[2],answers[3],answers[4]);
+            for(i=0; i<5; i++){
+                printf("Answer to question %d: %s\n", i, user_answer);
+                command = strtok(NULL, " ");
+                if(strcmp(command, answers[i])!=0)
+                    printf("Wrong answer on %d\n", i);
+                else printf("Right answer on %d\n", i);
+            }
         }
-        /* LIST */
+        /* RQT */
         else if(strcmp(command, "RQT")==0){
+            long QID, SID;
+            char time_limit[19];
 
-            /*TODO - verificar conteudo do RQT <iiiii>*/
+            /*GET SID*/
+            read_buffer = tcpread_until_char(new_fd, "\n", 6, 1);
+            SID = atoi(read_buffer);
+            printf("DEBUG: SID is: %d\n", SID);
+            fprintf(user_info_ptr, "%d ", SID);
 
+            /*GENERATE QID*/
+            srand (time(NULL));
+            QID = rand() % 900000 + 100000;
+            printf("DEBUG: QID is: %ld\n", QID);
+            fprintf(user_info_ptr, "%ld ", QID);
+
+            /*GET Current Time*/
+            strcpy(time_limit,get_time(600));
+            printf("DEBUG: Time limit is: %ld\n", QID);
+            fprintf(user_info_ptr, "%s\n", QID);
+
+            /*Transmission Stuff*/
             fseek(file_ptr, 0, SEEK_END);
             file_size = ftell(file_ptr);
             rewind(file_ptr);
 
-            /* TODO - ir buscar o numero do request e a hora */
-
-            strcpy(write_buffer, "AQT 12345_09JAN2015 09JAN2015_20:00:00 ");
+            strcpy(write_buffer, "AQT ");
+            strcat(write_buffer, QID);
+            strcat(write_buffer, " ");
+            strcat(write_buffer, time_limit);
+            strcat(write_buffer, " ");
             sprintf(buffer, "%d", file_size);
             strcat(write_buffer, buffer);
             strcat(write_buffer, " ");
@@ -115,8 +167,13 @@ int main(int argc, char *argv[]){
                 printf("Sending file... %d bytes sent\n", file_size-bytes_left);
             }
         }
+        else{
+            printf("Can't Recognize Transmission");
+        }
     }
     fclose(file_ptr);
+    fclose(answers_ptr);
+    fclose(user_info_ptr);
     close(sock_fd);
     close(new_fd);
 }
